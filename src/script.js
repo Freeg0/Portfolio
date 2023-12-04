0abcb9022e5eee904c5980d03310f1e456236766
Freeg0 <julien.mori.pro@gmail.com>
2023-12-04 20:28:21 +0100

    implementation ripple shaders

--- src/script.js
+++ src/script.js
@@ -1,70 +1,80 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
-import waterVertexShader from './shaders/water/vertex.glsl'
-import waterFragmentShader from './shaders/water/fragment.glsl'
+import waterVertexShader from './shaders/waterRipple/vertex.glsl'
+import waterFragmentBufferShader from './shaders/waterRipple/buffer_a_fragment.glsl'
+import waterFragmentShader from './shaders/waterRipple/final_fragment.glsl'
 
-/**
- * Base
- */
-// Debug
-const gui = new GUI()
+class BufferShader {
+  constructor(fragmentShader, uniforms = {}, width = 1, height = 1) {
+    this.uniforms = uniforms
+    this.material = new THREE.ShaderMaterial({
+      fragmentShader: fragmentShader,
+      vertexShader: waterVertexShader,
+      uniforms: uniforms,
+    })
+    this.scene = new THREE.Scene()
+    this.scene.add(
+      new THREE.Mesh(
+        new THREE.PlaneGeometry(width, height, 1, 1),
+        this.material
+      )
+    )
+  }
+}
+
+class BufferManager {
+  constructor(renderer, size) {
+    this.renderer = renderer
+
+    this.readBuffer = new THREE.WebGLRenderTarget(size.width, size.height, {
+      minFilter: THREE.LinearFilter,
+      magFilter: THREE.UVMapping,
+      format: THREE.RGBAFormat,
+      type: THREE.FloatType,
+      stencilBuffer: true,
+    })
+
+    this.writeBuffer = this.readBuffer.clone()
+  }
+
+  swap() {
+    const temp = this.readBuffer
+    this.readBuffer = this.writeBuffer
+    this.writeBuffer = temp
+  }
+
+  render(scene, camera, toScreen = false) {
+    if (toScreen) {
+      this.renderer.render(scene, camera)
+    } else {
+      this.renderer.setRenderTarget(this.writeBuffer)
+      this.renderer.clear()
+      this.renderer.render(scene, camera)
+      this.renderer.setRenderTarget(null)
+    }
+    this.swap()
+  }
+}
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
-
-// Scene
-const scene = new THREE.Scene()
 
 /**
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
-const channel0 = textureLoader.load('/textures/voronoise_3.png')
-const channel1 = textureLoader.load('/textures/ichannel1.png')
-const channel2 = textureLoader.load('/textures/ichannel2.png')
+const channel1 = textureLoader.load('/textures/test.jpg')
 
 /**
- * Test mesh
+ * Event Listener
  */
-// Geometry
-const geometry = new THREE.PlaneGeometry(6, 5, 512, 512)
-
-// Material
-const material = new THREE.ShaderMaterial({
-  vertexShader: waterVertexShader,
-  fragmentShader: waterFragmentShader,
-  uniforms: {
-    iTime: { value: 0 },
-    iTimeDelta: { value: 0 },
-    iFrameRate: { value: 60 },
-    iFrame: { value: 0 },
-    iChannel0: { value: channel0 },
-    iChannel1: { value: channel1 },
-    iChannel2: { value: channel2 },
-  },
-})
-
-material.uniforms.iChannel0.value.wrapS =
-  material.uniforms.iChannel0.value.wrapT = THREE.RepeatWrapping
-material.uniforms.iChannel1.value.wrapS =
-  material.uniforms.iChannel1.value.wrapT = THREE.RepeatWrapping
-material.uniforms.iChannel2.value.wrapS =
-  material.uniforms.iChannel2.value.wrapT = THREE.RepeatWrapping
-
-// Mesh
-const mesh = new THREE.Mesh(geometry, material)
-// mesh.rotation.x = -Math.PI * 0.5
-scene.add(mesh)
-
-/**
- * Sizes
- */
+let counter = 0
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
 }
-
+const mousePosition = new THREE.Vector4()
 window.addEventListener('resize', () => {
   // Update sizes
   sizes.width = window.innerWidth
@@ -74,27 +84,33 @@ window.addEventListener('resize', () => {
   camera.aspect = sizes.width / sizes.height
   camera.updateProjectionMatrix()
 
+  // Update render target sizes
+  targetA.setSize(window.innerWidth, window.innerHeight)
+  targetB.setSize(window.innerWidth, window.innerHeight)
+
   // Update renderer
   renderer.setSize(sizes.width, sizes.height)
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
+window.addEventListener('mousedown', () => {
+  mousePosition.setZ(1)
+  counter = 0
+})
+
+window.addEventListener('mouseup', () => {
+  mousePosition.setZ(0)
+})
+
+window.addEventListener('mousemove', (event) => {
+  mousePosition.setX(event.clientX)
+  mousePosition.setY(sizes.height - event.clientY)
+})
+
 /**
  * Camera
  */
-// Base camera
-const camera = new THREE.PerspectiveCamera(
-  75,
-  sizes.width / sizes.height,
-  0.3,
-  100
-)
-camera.position.set(0.25, -0.25, 1)
-scene.add(camera)
-
-// Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
+const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1)
 
 /**
  * Renderer
@@ -105,22 +121,59 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
+const targetA = new BufferManager(renderer, {
+  width: sizes.width,
+  height: sizes.height,
+})
+
+const targetB = new BufferManager(renderer, {
+  width: sizes.width,
+  height: sizes.height,
+})
+
+const uResolution = new THREE.Vector3(
+  sizes.width,
+  sizes.height,
+  window.devicePixelRatio
+)
+
+// Material
+const materialBufferA = new BufferShader(
+  waterFragmentBufferShader,
+  {
+    uResolution: {
+      value: uResolution,
+    },
+    uMousePosition: { value: mousePosition },
+    uChannel0: { value: null },
+    uFrame: { value: 0 },
+  },
+  sizes.width,
+  sizes.height
+)
+
+const materialImage = new BufferShader(waterFragmentShader, {
+  uResolution: {
+    value: uResolution,
+  },
+  uChannel0: { value: null },
+  uChannel1: { value: channel1 },
+})
+
 /**
  * Animate
  */
-const clock = new THREE.Clock()
-
 const tick = () => {
-  const elapsedTime = clock.getElapsedTime()
-
   // Update material
-  material.uniforms.iTime.value = elapsedTime
+  materialBufferA.uniforms['uFrame'].value = counter++
 
-  // Update controls
-  // controls.update()
+  materialBufferA.uniforms['uChannel0'].value = targetA.readBuffer.texture
+  targetA.render(materialBufferA.scene, camera)
 
-  // Render
-  renderer.render(scene, camera)
+  materialImage.uniforms['uChannel0'].value = targetA.readBuffer.texture
+  targetB.render(materialImage.scene, camera, true)
+  console.log(mousePosition)
+  console.log(window.innerHeight)
 
   // Call tick again on the next frame
   window.requestAnimationFrame(tick)
